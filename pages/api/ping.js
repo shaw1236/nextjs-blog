479f6469a06446a609d9c080d3a001986159d29c
Simon Li <SimonL@vizetto.com>
2022-07-04 14:18:42 -0400

    revised api "ping"

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -5,20 +5,19 @@ import { getCallerIP, ipLookUp } from '../../lib/tools';
 export default async function ping(req, res) {
     try {
         const IPAddress = getCallerIP(req);
-        if (req.url.indexOf('?') < 0) {
+        const { ip } = req.query; 
+        if (!ip) {
             return res.send(await ipLookUp(IPAddress) || { message: "Local run or no data found" });
         }
-        const { ip } = req.query; 
-        if (ip) {
-            const isIP = Net.isIP(ip); 
-            if (isIP) {
-                const IPAddr = isIP? ip : IPAddress;  
-                res.send(await ipLookUp(IPAddr) || { message: "No data found" });         
-            }
-            else { 
-                res.send(`My IP Address is: ${IPAddress}`);
-            }
-        }    
+        
+        const isIP = Net.isIP(ip); 
+        if (isIP) {
+            const IPAddr = isIP? ip : IPAddress;  
+            res.send(await ipLookUp(IPAddr) || { message: "No data found" });         
+        }
+        else { 
+            res.send(`My IP Address is: ${IPAddress}`);
+        }
     }
     catch (ex) {
         res.send({ result: false, message: ex.message || ex });
